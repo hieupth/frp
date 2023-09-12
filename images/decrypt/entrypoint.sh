0abff00fae00399d74c4fbf84e55fb72f466d99b
hieupth <64821726+hieupth@users.noreply.github.com>
2023-09-12 15:37:12 +0700

    chore(decrypt): fix build error

--- images/decrypt/entrypoint.sh
+++ images/decrypt/entrypoint.sh
@@ -5,13 +5,13 @@ exec "$@"
 echo "$PASSWORD" > password
 # Pull certs git repo if exist.
 if [[ -z "${TLS_GIT_REPO}" ]]; then
-  if cd /certs; then git pull; else git clone ${TLS_GIT_REPO} /certs; fi
-  ansible-vault decrypt --vault-password-file password \
-    /certs/${FRP_HOST_DOMAIN}/fullchain.pem \
-    /certs/${FRP_HOST_DOMAIN}/privkey.pem \
-    /certs/${FRP_HOST_DOMAIN}/cert.pem \
-    /certs/${FRP_HOST_DOMAIN}/chain.pem \
-    /certs/${FRP_HOST_DOMAIN}/concat.pem 
-fi 
+  if cd /ssl; then git pull; else mkdir -p /ssl && git clone ${TLS_GIT_REPO} /ssl; fi
+fi  
+ansible-vault decrypt --vault-password-file password \
+  /ssl/certs/${FRP_HOST_DOMAIN}/fullchain.pem \
+  /ssl/certs/${FRP_HOST_DOMAIN}/privkey.pem \
+  /ssl/certs/${FRP_HOST_DOMAIN}/cert.pem \
+  /ssl/certs/${FRP_HOST_DOMAIN}/chain.pem \
+  /ssl/certs/${FRP_HOST_DOMAIN}/concat.pem 
 # Run frp app
 frp -c ${FRP_CONFIG_FILE:-/etc/frp/frp.ini}
