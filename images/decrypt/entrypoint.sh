0107faa4f5427e0bcbe5d7e357da302f30faeca6
hieupth <64821726+hieupth@users.noreply.github.com>
2023-09-13 09:35:50 +0700

    chore(decrypt): fix entrypoint

--- images/decrypt/entrypoint.sh
+++ images/decrypt/entrypoint.sh
@@ -2,16 +2,29 @@
 set -e
 exec "$@"
 # Save decrypt password.
-echo "$PASSWORD" > password
+echo "$PASSWORD" > /password
 # Pull certs git repo if exist.
-if [[ -z "${TLS_GIT_REPO}" ]]; then
-  if cd /ssl; then git pull; else mkdir -p /ssl && git clone ${TLS_GIT_REPO} /ssl; fi
+if [[ -z "${TLS_GIT_REPO}" ]] 
+then
+  echo "Ignore git"
+else
+  echo "Git clone: ${TLS_GIT_REPO}"
+  if cd /ssl 
+  then 
+    git pull 
+  else 
+    cd /
+    git clone ${TLS_GIT_REPO} /ssl 
+  fi
 fi  
-ansible-vault decrypt --vault-password-file password \
+# Ansible decrypt certs.
+ansible-vault decrypt --vault-password-file /password \
   /ssl/certs/${FRP_HOST_DOMAIN}/fullchain.pem \
   /ssl/certs/${FRP_HOST_DOMAIN}/privkey.pem \
   /ssl/certs/${FRP_HOST_DOMAIN}/cert.pem \
   /ssl/certs/${FRP_HOST_DOMAIN}/chain.pem \
   /ssl/certs/${FRP_HOST_DOMAIN}/concat.pem 
+# Fix certs permission.
+chmod 600 /ssl/certs/${FRP_HOST_DOMAIN}/*.pem
 # Run frp app
 frp -c ${FRP_CONFIG_FILE:-/etc/frp/frp.ini}
